379fe9268c66952128629d18a0d0a4a86013b785
girtri <girtri@libero.it>
2017-04-08 17:21:00 +0200

    init project

---
+++ testsql.php
@@ -0,0 +1,49 @@
+<?php
+$serverName = "Gerry_win81";
+//$serverName = "192.168.56.1";
+
+$connectionInfo = array(
+    "Database" => "Test_01",
+    "Uid" => "sa",
+    "PWD" => "sqlmetodo"
+);
+
+
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if (!$conn)
+    die(FormatErrors(sqlsrv_errors()));
+
+//Select Query
+//$tsql= "SELECT * FROM TabZone";
+$tsql= "SELECT @@Version as SQL_VERSION";
+
+//Executes the query
+$getResults= sqlsrv_query($conn, $tsql);
+
+//Error handling 
+if ($getResults == FALSE)
+    die(FormatErrors(sqlsrv_errors()));
+?> 
+
+<h1> Results : </h1>
+
+<?php
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    echo ($row['SQL_VERSION']);
+    echo ("<br/>");
+}
+
+sqlsrv_free_stmt($getResults);
+function FormatErrors( $errors )  
+{  
+    /* Display errors. */  
+    echo "Error information: <br/>";  
+  
+    foreach ( $errors as $error ) {  
+        echo "SQLSTATE: ".$error['SQLSTATE']."<br/>";  
+        echo "Code: ".$error['code']."<br/>";  
+        echo "Message: ".$error['message']."<br/>";  
+    }  
+}  
+?>
